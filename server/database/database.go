d1b9dbcbe97daba27b702e5344f6235703c4bcba
Tripti Verma <vermatripti547@gmail.com>
2025-08-22 13:53:44 +0530

    some minor changes

--- server/database/database.go
+++ server/database/database.go
@@ -1,37 +1,3 @@
-// package database
-
-// import (
-// 	"log"
-// 	"os"
-
-// 	"github.com/Triptiverma003/blog/model"
-// 	"gorm.io/driver/mysql"
-// 	"gorm.io/gorm"
-// 	"gorm.io/gorm/logger"
-// )
-
-// var DBConn *gorm.DB
-
-// func ConnectDB() {
-// 	user := os.Getenv("db_username")
-// 	password := os.Getenv("db_password")
-// 	dbname := os.Getenv("db_name")
-
-// 	dsn := user + ":" + password + "@tcp(127.0.0.1:3306)/" + dbname + "?charset=utf8mb4&parseTime=True&loc=Local"
-
-// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-// 		Logger: logger.Default.LogMode(logger.Error),
-// 	})
-
-// 	if err != nil {
-// 		panic("Database is not connected!")
-// 	}
-// 	log.Println("Connection Successful")
-
-// 	db.AutoMigrate(new(model.Blog))
-// 	DBConn = db
-// }
-
 package database
 
 import (
@@ -39,6 +5,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/joho/godotenv" // load .env locally
 	"github.com/Triptiverma003/blog/model"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -48,27 +15,36 @@ import (
 var DBConn *gorm.DB
 
 func ConnectDB() {
+	// Load .env file for local dev (ignore error in prod)
+	_ = godotenv.Load()
+
 	user := os.Getenv("db_username")
 	password := os.Getenv("db_password")
 	dbname := os.Getenv("db_name")
 	host := os.Getenv("db_hostname")
-	port := os.Getenv("port_number") // Use this
+	port := os.Getenv("port_number")
 
-	// ✅ Build DSN using host and port
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	if user == "" || password == "" || dbname == "" || host == "" || port == "" {
+		log.Fatal("❌ Missing database environment variables")
+	}
+
+	// ✅ Railway often requires TLS
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local&tls=true",
 		user, password, host, port, dbname)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Error),
 	})
-
 	if err != nil {
 		log.Fatal("❌ Database connection failed:", err)
 	}
+
 	log.Println("✅ Database connection successful")
 
 	// Auto migrate models
-	db.AutoMigrate(&model.Blog{})
+	if err := db.AutoMigrate(&model.Blog{}); err != nil {
+		log.Fatal("❌ Migration failed:", err)
+	}
 
 	DBConn = db
 }
